96b23a147b6765e4a441ca9a9c8bb3b025b22906
Guilherme Zühlke O'Connor <guioconnor@guioconnor.com>
2022-03-25 15:06:19 +0000

    Setting up inputs

--- src/action.js
+++ src/action.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
-
+const core = require('@actions/core')
 
 async function run() {
+  const character = core.getInput('character');
+  console.log('c:' + character);
+
   const {data} = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1')
 
   console.log(`${data[0].character}: ${data[0].quote}`);
