696c2a42fb2840007e1d4414f903aafddd279f94
Guilherme Zühlke O'Connor <guioconnor@guioconnor.com>
2022-03-25 15:09:11 +0000

    Using input character into quote query

--- src/action.js
+++ src/action.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 const core = require('@actions/core')
 
-async function run() {
-  const character = core.getInput('character');
-  console.log('c:' + character);
+const DEFAULT_CHARACTER = 'dr-zoidberg';
 
-  const {data} = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1')
+async function run() {
+  const character = core.getInput('character') || DEFAULT_CHARACTER;
+  console.log(character);
+
+  const {data} = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${character}/1`)
 
   console.log(`${data[0].character}: ${data[0].quote}`);
 
