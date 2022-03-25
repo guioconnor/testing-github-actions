a62f898c338e9ec7cda919a294951c59cd9d3b8a
Guilherme Zühlke O'Connor <guioconnor@guioconnor.com>
2022-03-25 16:18:03 +0000

    debug logs

--- src/action.js
+++ src/action.js
@@ -13,12 +13,19 @@ const AVAILABLE_CHARACTERS = [
 async function run() {
   const character = core.getInput('character') || DEFAULT_CHARACTER;
 
+  core.debug(`[Futurama] Input character: ${character}`);
+
   if( !AVAILABLE_CHARACTERS.includes(character)){
     core.setFailed(`Unknown character: ${character}`)
     return
   }
 
+  core.debug(`[Futurama] Retrieving quote for: ${character}`)
+
   const {data} = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${character}/1`)
+
+  core.debug(`[Futurama] Data: ${JSON.stringify(data)}`)
+
   const firstEntry = data[0];
 
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
